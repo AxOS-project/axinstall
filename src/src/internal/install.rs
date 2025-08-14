342fa5d41d5e99ba5405a8f1ad337a0822825d3a
Ardox <ardox@axos-project.com>
2025-08-14 10:28:58 +0200

    Testing a new installation logic

--- src/src/internal/install.rs
+++ src/src/internal/install.rs
@@ -4,7 +4,12 @@ use std::process::Command;
 
 pub fn install(pkgs: Vec<&str>) {
     exec_eval(
-        Command::new("pacstrap").arg("/mnt").args(&pkgs).status(),
+        Command::new("pacman")
+        .arg("--root=/mnt")
+        .arg("--cachedir=/var/cache/pacman/pkg")
+        .arg("--noconfirm")
+        .args(&pkgs)
+        .status(),
         format!("Install packages {}", pkgs.join(", ")).as_str(),
     );
     // umount("/mnt/dev");
